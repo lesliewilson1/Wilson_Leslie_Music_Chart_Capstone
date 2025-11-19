eabba455c72282cc78958a900963d86015337c40
Leslie <wilson.leslie2021@gmail.com>
2025-11-19 14:49:40 -0500

    Update 3.4

--- backend/index.js
+++ backend/index.js
@@ -17,24 +17,43 @@ app.get('/', (req, res) => {
   res.json('Server Working');
 });
 
-//----------------------------------3rd Party API Route----------------------------------------------//
+//----------------------------------3rd party API route----------------------------------------------//
 
+//********************************Fetch Leslie's Top 15 Tracks***************************************//
 app.get('/tracks', async (req, res) => {
 
+  try {
     const accessToken = await spotifyToken();
-
-    //Fetch Leslie's Top 50 Charts
     const playlistId = '5N6oTfw4CiGREvmDH5WfAD';
     const trackResponse = await axios.get(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, {
       headers: { Authorization: `Bearer ${accessToken}` }
     });
-  
-    res.json(trackResponse.data);
+
+ //Filter API data to display clean data for frontend
+    const filteredTracks = trackResponse.data.items.map(item => {
+      const track = item.track;
+      return {
+        track_id: track.id,
+        track_name: track.name,
+        artist_name: track.artists[0]?.name || 'Unknown', //optional chaining
+        image_url: track.album.images[1]?.url || '',
+        spotify_url: track.external_urls.spotify,
+        popularity: track.popularity,
+        added_at: item.added_at
+      };
+    });
+
+    res.json(filteredTracks);
+     } catch(e) {
+      console.log('Error fetch from API:', e.trackResponse)
+      res.status(500).json({e: 'Error fetch from API'}) //display error in frontend
+     }
   });
+ 
 
 
 
-//-------------------------------------Listening---------------------------------------------------//
+//-------------------------------------Listening/connectDB---------------------------------------------//
 
 app.listen(port, () => {
   console.log('Listening Port: ', port);
