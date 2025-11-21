3a8c08c75aacda799227641cf45c5eebd649261f
Leslie <wilson.leslie2021@gmail.com>
2025-11-21 14:59:00 -0500

    Update 5.2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Nav from './components/Nav/Nav'
 import Home from './pages/Home'
 import About from './pages/About'
@@ -18,7 +18,7 @@ function App() {
                 <Route path="/about" element={<About />}/>
                 <Route path="/form" element={<Form />}/>
                 <Route path="/tracks" element={<Tracks />}/>
-                <Route path="*" element={<Navigate to='/' />}/>
+                {/* <Route path="*" element={<Navigate to='/' />}/> */}
             </Routes>
         </Router>
     )
--- frontend/src/pages/Form.jsx
+++ frontend/src/pages/Form.jsx
@@ -1,12 +1,32 @@
+import React from 'react'
+
 function Form() {
 
     return (
         <>
         <div>
-            <Form />
+            <section className="contact">
+                <form>
+                    <h2>Track Request</h2>
+                    <div className="input-box">
+                        <label>Track Name</label>
+                        <input type="text" className="field" placeholder='Enter track name' required />
+                    </div>
+                    <div className="input-box">
+                        <label>Artist Name</label>
+                        <input type="text" className="field" placeholder='Enter artist name' required />
+                    </div>
+                    <div className="input-box">
+                        <label>Year</label>
+                        <input type="text" className="field" placeholder='Enter year name' required />
+                    </div>
+                    <button type="submit">Send</button>
+                </form>
+            </section>
+          
         </div>
         </>
     )
 }
 
-export default Form
+export default Form;
