f2f73cd6aa72ae92a3dd5a6a3e9eb475ab4bb5c7
Leslie <wilson.leslie2021@gmail.com>
2025-11-21 09:23:09 -0500

    Update 5.0

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,49 @@
-import { useState, useEffect } from 'react'
+import { Router, Routes, Route, Navigate } from 'react-dom'
+import Nav from './components/Nav'
+import Home from './pages/Home'
+import About from './pages/About'
+import Form from './pages/Form'
+import Tracks from './pages/Tracks'
+
 import './App.css'
 
 
 function App() {
 
-const [tracks, setTracks] = useState([])
+    return (
+        <Router>
+            <Nav />
+            <Routes>
+                <Route path="/" element={<Home />}/>
+                <Route path="/about" element={<About />}/>
+                <Route path="/form" element={<Form />}/>
+                <Route path="/tracks" element={<Tracks />}/>
+                <Route path="*"element={<Navigate to='/' />} />
+            </Routes>
+        </Router>
+    )
     
-//Connected backend
-useEffect(() => {
-    async function getTracks() {
-        const response = await fetch('http://localhost:8080/trackrequest')
-        const data = await response.json()
-        console.log(data)
-        setTracks(data)
-    }
-    getTracks()
-}, [])
-
-
 
 }
 
 export default App
+
+
+
+
+
+
+// const [tracks, setTracks] = useState([])
+    
+// //Connected backend
+// useEffect(() => {
+//     async function getTracks() {
+//         const response = await fetch('http://localhost:8080/trackrequest')
+//         const data = await response.json()
+//         console.log(data)
+//         setTracks(data)
+//     }
+//     getTracks()
+// }, [])
+
+
