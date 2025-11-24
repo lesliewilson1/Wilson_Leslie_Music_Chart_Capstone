85f09e34eda0841c35228a0475236e0cbccab63f
Leslie <wilson.leslie2021@gmail.com>
2025-11-24 14:23:25 -0500

    Update 7.2

---
+++ frontend/src/components/ball.jsx
@@ -0,0 +1,29 @@
+import './ball.css'
+
+function Ball() {
+    return (
+        
+                    <div className="main">
+                <div className="shape-container move-right1">
+                    <div className="shape blue-ball bounce1">
+                    </div>
+                </div>
+                <div className="shape-container move-left2">
+                    <div className="shape red-ball bounce2">
+                    </div>
+                </div>
+                <div className="shape-container move-right4">
+                    <div className="shape green-ball bounce4">
+                    </div>
+                </div>
+                <div className="shape-container move-left3">
+                    <div className="shape blue-ball bounce3">
+                    </div>
+                </div>
+
+            </div>
+        
+    )
+}
+
+export default Ball
--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,24 +1,27 @@
+import './About.css'
+import Ball from '../components/ball'
+
 function About() {
 
     return (
         <>
         <div>
             <section className="about">
-                <div>
-                    <h1 className="about-header">About</h1>
+                <div className="about-title"><h1><u>About</u></h1></div>
+                <div className="about-content">
+                    <div className="about-para">
+                        <h1>Popular</h1>
+                            <h3>(paa·pyuh·lr)</h3> <br></br>
+                            <h3>What defines popular music in today's modern era?</h3> <br></br>
+                            <h2>We have the answer.</h2>
+                            <p>POP (popular) music can be electronic, r&b, bubblegum, hyper, etc.</p>
+                            <p>We created a special formula to show popular music across genres in the U.S</p>
+                     </div>
                 </div>
-                <div>
-                <h1>Popular</h1>
-                    <h2>(paa·pyuh·lr)</h2>
-                    <h3>What defines popular music in today's modern era?</h3>
-                    <h2>We have the answer.</h2>
-                    <p>POP (popular) music can be electronic, r&b, bubblegum, hyper, etc.</p>
-                    <p>We created a special formula to show popular music across genres in the U.S</p>
-                </div>
-
             </section>
+
         </div>
-        
+    <Ball></Ball>
         </>
     )
 }
